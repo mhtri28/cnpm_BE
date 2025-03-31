3ff5a3ba8c1856e4910cdc1095cec7a769b2ff96
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-31 13:43:30 +0700

    refactor: versioning api

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Thêm global prefix cho API v1
+  app.setGlobalPrefix('api/v1');
+
+  // Thêm validation pipe
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
