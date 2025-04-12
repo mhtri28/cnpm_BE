ba489610c4d8db02f54aed853db6edd7d5146e49
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-12 15:22:57 +0700

    docs(tables): enhance documentation for Tables

--- src/modules/tables/entities/table.entity.ts
+++ src/modules/tables/entities/table.entity.ts
@@ -8,24 +8,50 @@ import {
   OneToMany,
 } from 'typeorm';
 import { Order } from '../../orders/entities/order.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('tables')
 export class Table {
+  @ApiProperty({
+    description: 'ID của bàn',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  @Column({ type: 'varchar', length: 36, unique: true })
   @PrimaryColumn()
   id: string;
 
-  @Column()
+  @ApiProperty({
+    description: 'Tên của bàn',
+    example: 'Bàn 1',
+  })
+  @Column({ unique: true })
   name: string;
 
+  @ApiProperty({
+    description: 'Thời gian tạo lúc',
+    example: '2023-04-01T08:30:00Z',
+  })
   @CreateDateColumn()
   createdAt: Date;
 
+  @ApiProperty({
+    description: 'Thời gian cập nhật gần nhất lúc',
+    example: '2023-04-15T10:45:00Z',
+  })
   @UpdateDateColumn({ nullable: true })
   updatedAt: Date;
 
+  @ApiProperty({
+    description: 'Thời gian xóa lúc',
+    example: null,
+  })
   @DeleteDateColumn({ nullable: true })
   deletedAt: Date;
 
+  @ApiProperty({
+    description: 'Trạng thái của bàn',
+    example: 'available',
+  })
   @OneToMany(() => Order, (order) => order.table)
   orders: Order[];
 }
